0ea29e8345c28546947714a01cf9fa66579ef300
sc-rain <130538093+sc-rain@users.noreply.github.com>
2023-04-13 17:36:51 +0900

    ssl対応していないアクセスカウンタ―だったので戻した

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -6,39 +6,12 @@
     </v-app-bar-title>
 
     <v-responsive max-width="100">
-      <table border="0" cellspacing="0" cellpadding="0">
-        <tr>
-          <td align="center">
-            <a href="http://www.rays-counter.com/"
-              ><img
-                src="http://www.rays-counter.com/d1264_f4_237/6437b9cabf767/"
-                alt="アクセスカウンター"
-                border="0"
-            /></a>
-          </td>
-        </tr>
-        <tr>
-          <td align="center">
-            <img
-              src="http://www.rays-counter.com/images/counter_01.gif"
-              border="0"
-            /><img
-              src="http://www.rays-counter.com/images/counter_02.gif"
-              border="0"
-            /><img
-              src="http://www.rays-counter.com/images/counter_03.gif"
-              border="0"
-              alt=""
-            /><img
-              src="http://www.rays-counter.com/images/counter_04.gif"
-              border="0"
-            /><img
-              src="http://www.rays-counter.com/images/counter_05.gif"
-              border="0"
-            />
-          </td>
-        </tr>
-      </table>
+      <a href="https://www.free-counter.jp/"
+        ><img
+          src="https://www.f-counter.net/j/61/1681356105/"
+          alt="アクセスカウンター"
+          onerror="this.onerror = null; this.src='';"
+      /></a>
     </v-responsive>
   </v-app-bar>
 </template>
