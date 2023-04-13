1943345203f39ea57d45698b384109eb1b841ac1
sc-rain <130538093+sc-rain@users.noreply.github.com>
2023-04-13 17:28:10 +0900

    アクセスカウンター変更

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,14 +2,15 @@
   <v-container class="fill-height">
     <v-responsive class="d-flex align-center text-center fill-height">
       ようこそ<br />
-      あなたは<a href="https://www.free-counter.jp/"
-        ><img
-          src="https://www.f-counter.net/j/61/1681356105/"
-          alt="アクセスカウンター" /></a
-      >人目の訪問者です
+      <br />
+      [rain-note]<br />
+      あめのおと<br />
       <br />
       <v-img contain height="128" src="@/assets/logo.png" />
-      
+      <br />
+      [工事中...]<br />
+      <a href="https://twpf.jp/_SouthernCross_">セッションログ</a>とか、追々引っ越してきます。
+
     </v-responsive>
   </v-container>
 </template>
--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -4,5 +4,41 @@
       <v-icon icon="mdi-umbrella" />
       [rain-notes] あめのおと
     </v-app-bar-title>
+
+    <v-responsive max-width="100">
+      <table border="0" cellspacing="0" cellpadding="0">
+        <tr>
+          <td align="center">
+            <a href="http://www.rays-counter.com/"
+              ><img
+                src="http://www.rays-counter.com/d1264_f4_237/6437b9cabf767/"
+                alt="アクセスカウンター"
+                border="0"
+            /></a>
+          </td>
+        </tr>
+        <tr>
+          <td align="center">
+            <img
+              src="http://www.rays-counter.com/images/counter_01.gif"
+              border="0"
+            /><img
+              src="http://www.rays-counter.com/images/counter_02.gif"
+              border="0"
+            /><img
+              src="http://www.rays-counter.com/images/counter_03.gif"
+              border="0"
+              alt=""
+            /><img
+              src="http://www.rays-counter.com/images/counter_04.gif"
+              border="0"
+            /><img
+              src="http://www.rays-counter.com/images/counter_05.gif"
+              border="0"
+            />
+          </td>
+        </tr>
+      </table>
+    </v-responsive>
   </v-app-bar>
 </template>
