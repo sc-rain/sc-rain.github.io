70240a75f60836d0dfcebddc5d3cecb670a6d8ad
sc-rain <130538093+sc-rain@users.noreply.github.com>
2023-04-13 12:32:18 +0900

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container class="fill-height">
+    <v-responsive class="d-flex align-center text-center fill-height">
+      ようこそ<br />
+      あなたは<a href="https://www.free-counter.jp/"
+        ><img
+          src="https://www.f-counter.net/j/61/1681356105/"
+          alt="アクセスカウンター" /></a
+      >人目の訪問者です
+      <br />
+      <v-img contain height="128" src="@/assets/logo.png" />
+      
+    </v-responsive>
+  </v-container>
+</template>
---
+++ src/layouts/default/AppBar.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-app-bar flat>
+    <v-app-bar-title>
+      <v-icon icon="mdi-umbrella" />
+      [rain-notes] あめのおと
+    </v-app-bar-title>
+  </v-app-bar>
+</template>
